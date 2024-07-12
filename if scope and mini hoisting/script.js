fdc620f50c5efac05318ec4cf8ff12573ca24f55
fawadsw20 <fawadhassan.sw1997@gmail.com>
2024-07-12 11:31:33 +0500

    Add files via upload

---
+++ if scope and mini hoisting/script.js
@@ -0,0 +1,14 @@
+
+if(true){
+
+const username= "fawad"
+
+    if(username === "fawad") {
+        const website = "youtube"
+        console.log(`Hello ${username}` +` Watch this course on ${website}`);
+    }
+
+    // console.log(website);
+}
+
+// console.log(username);
